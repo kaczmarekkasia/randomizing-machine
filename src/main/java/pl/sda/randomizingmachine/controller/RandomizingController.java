a5f47aa08eb1dab2c5fcf6b86b2e03294da02245
Kasia Kaczmarek <katarzyna.dobrzeniecka@gmail.com>
2019-10-22 18:55:39 +0200

    random words and numbers

---
+++ src/main/java/pl/sda/randomizingmachine/controller/RandomizingController.java
@@ -0,0 +1,28 @@
+package pl.sda.randomizingmachine.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class RandomizingController {
+
+    @Value("${random.words}")
+    private String[] words;
+
+    @Value("${random.number}")
+    private String[] numbers;
+
+    @GetMapping("/word")
+    public String randomWord(){
+        return words[new Random().nextInt(words.length)] ;
+    }
+
+    @GetMapping("/number")
+    public String randomNumber(){
+        return numbers[new Random().nextInt(numbers.length)];
+    }
+}
